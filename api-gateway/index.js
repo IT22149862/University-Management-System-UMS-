1435c9f18dd105611d5d58912039385c38712ce4
Nishini Silva <nishinihimasha@gmail.com>
2026-03-31 07:08:20 +0530

    error file
    error

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -4,7 +4,6 @@ const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const app = express();
-app.use(express.json());
 // app.use(express.json());
 
 // ── Service URLs ─────────────────────────────────────────────────────────────
